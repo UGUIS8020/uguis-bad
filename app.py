c51109f6dd2fe64b8466f8a77e3222d25e3d0b4a
UGUIS8020 <shibuyamasahiko@gmail.com>
2025-06-29 16:14:13 +0900

    新越谷

--- app.py
+++ app.py
@@ -1412,6 +1412,7 @@ def update_skill_score():
 dynamodb = boto3.resource("dynamodb", region_name="ap-northeast-1")
 match_table = dynamodb.Table("bad-game-match_entries")
 
+
 from uguu.timeline import uguu
 from uguu.users import users
 from schedule.views import bp as bp_schedule
